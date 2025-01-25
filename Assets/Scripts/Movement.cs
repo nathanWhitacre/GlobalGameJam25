9c9930ed89287ed48ba4db21099c1d9f26970daf
iyang2006 <yang2006@purdue.edu>
2025-01-24 21:17:21 -0500

    s

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        var movement = GetComponent<PlayerInput>().actions.FindAction("movement");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement
     }
 }
