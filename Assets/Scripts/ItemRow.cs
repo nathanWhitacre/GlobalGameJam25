0aee60f87ec9be187e3f887fd6c811449f54806a
theNareynater <whitacre@purdue.edu>
2025-01-25 20:05:51 -0500

    BIG COMMIT
    We got obstacles

--- Assets/Scripts/ItemRow.cs
+++ Assets/Scripts/ItemRow.cs
@@ -1,27 +1,144 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ItemRow : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D rigidBody;
-    [SerializeField] public float rowSpeed = 1f;
-    [SerializeField] public int hazardSpawnPercent = 20;
+    [HideInInspector] public float rowSpeed = 1f;
+
+    [HideInInspector] public float hazardSpawnPercent = 20f;
     [SerializeField] public GameObject hazard;
-    [SerializeField] public List<GameObject> items;
+
+    [HideInInspector] public float smallHazardSpawnPercent = 20f;
+    [SerializeField] public GameObject smallHazard;
+
+    [HideInInspector] public float bubbleSpawnPercent = 20f;
+    [SerializeField] public GameObject bubble;
+
+    [HideInInspector] public float fishSpawnPercent = 20f;
+    [SerializeField] public GameObject fish;
+    [SerializeField] public float maxFishSpawnHeight = 5f;
+    [SerializeField] public float minFishSpawnHeight = -2f;
+    [HideInInspector] public bool isSpawningFish = false;
+    [HideInInspector] public float fishSpawnHeight = 0f;
+
+    [HideInInspector] public List<GameObject> items;
+    [HideInInspector] public ItemRowSpawner itemRowSpawner;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0f, 10f, 0f);
-        items = new List<GameObject>(new GameObject[9]);
-        for (int i = 0; i < 9; i++)
+        itemRowSpawner = transform.parent.gameObject.GetComponent<ItemRowSpawner>();
+
+        //Set Spawn Rates
+        hazardSpawnPercent = itemRowSpawner.hazardSpawnPercent;
+        smallHazardSpawnPercent = itemRowSpawner.smallHazardSpawnPercent;
+        bubbleSpawnPercent = itemRowSpawner.bubbleSpawnPercent;
+        fishSpawnPercent = itemRowSpawner.fishSpawnPercent;
+
+        //Fish Spawn
+        isSpawningFish = false;
+        if (Random.Range(1f, 100f) <= fishSpawnPercent)
         {
-            if (Random.Range(1, 100) <= hazardSpawnPercent)
+            isSpawningFish = true;
+            fishSpawnHeight = Random.Range(minFishSpawnHeight, maxFishSpawnHeight);
+        }
+
+        //Generate Items
+        items = new List<GameObject>(new GameObject[9]);
+        for (int i = 0; i < items.Count; i++)
+        {
+
+            //Bubble Spawn
+            if (Random.Range(1f, 100f) <= bubbleSpawnPercent) //Spawn Chance
             {
-                float xPosition = -8f + (i * 2f);
-                items[i] = Instantiate(hazard, new Vector3(xPosition, transform.position.y, 0f), transform.rotation, transform);
+                //Anti-grouping Override
+                if (itemRowSpawner.previousRow != null)
+                {
+                    List<GameObject> previousItems = itemRowSpawner.previousRow.GetComponent<ItemRow>().items;
+                    if (//previousItems[Mathf.Clamp((i - 1), 0, (previousItems.Count - 1))] == null && //Previous Left
+                        //items[Mathf.Clamp((i - 1), 0, items.Count - 1)] == null && //Current Left
+                        //previousItems[i] == null && //Previous Center
+                        //previousItems[Mathf.Clamp((i + 1), 0, previousItems.Count - 1)] == null && //Previous Right
+                        true)
+                    {
+                        //Spawn Bubble
+                        float xPosition = -8f + (i * 2f) + Random.Range(-1f, 1f);
+                        float yPosition = transform.position.y + Random.Range(-1f, 1f);
+                        items[i] = Instantiate(bubble, new Vector3(xPosition, yPosition, 0f), transform.rotation, transform);
+                        continue;
+                    }
+
+                }
+            }
+
+            //Hazard Spawn
+            if (!isSpawningFish && Random.Range(1f, 100f) <= hazardSpawnPercent) //Spawn Chance
+            {
+                //Anti-grouping Override
+                if (itemRowSpawner.previousRow != null)
+                {
+                    List<GameObject> previousItems = itemRowSpawner.previousRow.GetComponent<ItemRow>().items;
+                    if (previousItems[Mathf.Clamp((i-1), 0, (previousItems.Count-1))] == null && //Previous Left
+                        items[Mathf.Clamp((i-1), 0, items.Count-1)] == null && //Current Left
+                        previousItems[i] == null && //Previous Center
+                        previousItems[Mathf.Clamp((i+1), 0, previousItems.Count-1)] == null && //Previous Right
+                        true)
+                    {
+                        //Spawn Hazard
+                        float xPosition = -8f + (i * 2f);
+                        items[i] = Instantiate(hazard, new Vector3(xPosition, transform.position.y, 0f), transform.rotation, transform);
+                        continue;
+                    }
+                    
+                }
+            }
+
+            //Small Hazard Spawn
+            if (!isSpawningFish && Random.Range(1f, 100f) <= smallHazardSpawnPercent) //Spawn Chance
+            {
+                //Anti-grouping Override
+                if (itemRowSpawner.previousRow != null)
+                {
+                    //Left Hazard
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        List<GameObject> previousItems = itemRowSpawner.previousRow.GetComponent<ItemRow>().items;
+                        if (i != 0 && //Wall Current left
+                            previousItems[Mathf.Clamp((i - 1), 0, (previousItems.Count - 1))] == null && //Previous Left
+                            items[Mathf.Clamp((i - 1), 0, items.Count - 1)] == null && //Current Left
+                            previousItems[i] == null && //Previous Center
+                            //previousItems[Mathf.Clamp((i + 1), 0, previousItems.Count - 1)] == null && //Previous Right
+                            true)
+                        {
+                            //Spawn Small Hazard
+                            float xPosition = -9f + (i * 2f);
+                            items[i] = Instantiate(smallHazard, new Vector3(xPosition, transform.position.y, 0f), transform.rotation, transform);
+                            continue;
+                        }
+                    }
+                    //Right Hazard
+                    else
+                    {
+                        List<GameObject> previousItems = itemRowSpawner.previousRow.GetComponent<ItemRow>().items;
+                        if (i != (items.Count-1) && //Wall Current left
+                            //previousItems[Mathf.Clamp((i - 1), 0, (previousItems.Count - 1))] == null && //Previous Left
+                            items[Mathf.Clamp((i - 1), 0, items.Count - 1)] == null && //Current Left
+                            previousItems[i] == null && //Previous Center
+                            previousItems[Mathf.Clamp((i + 1), 0, previousItems.Count - 1)] == null && //Previous Right
+                            true)
+                        {
+                            //Spawn Small Hazard
+                            float xPosition = -7f + (i * 2f);
+                            items[i] = Instantiate(smallHazard, new Vector3(xPosition, transform.position.y, 0f), transform.rotation, transform);
+                            continue;
+                        }
+                    }
+                }
             }
         }
     }
@@ -29,14 +146,34 @@ public class ItemRow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Set Speed
+        if (itemRowSpawner != null)
+        {
+            rowSpeed = itemRowSpawner.rowSpeed;
+        }
+
+        //Destroy Row
         if (transform.position.y < -10f)
         {
             Destroy(gameObject);
+        }
+
+        //Spawn Fish
+        if (isSpawningFish && transform.position.y < fishSpawnHeight)
+        {
+            isSpawningFish = false;
+            float xPosition = -10f;
+            if (Random.Range(0, 2) == 0)
+            {
+                xPosition = Mathf.Abs(xPosition);
+            }
+            Instantiate(fish, new Vector3(xPosition, transform.position.y, 0f), transform.rotation, transform);
         }
     }
 
     void FixedUpdate()
     {
+        //It GO
         rigidBody.velocity = new Vector2(0f, -1 * rowSpeed);
     }
 }
--- Assets/Scripts/ItemRowSpawner.cs
+++ Assets/Scripts/ItemRowSpawner.cs
@@ -6,11 +6,36 @@ using UnityEngine;
 public class ItemRowSpawner : MonoBehaviour
 {
 
-    [SerializeField] public int spawnHeight = 10;
-    [SerializeField] public float rowSpeed = 1f;
-    [SerializeField] public int hazardSpawnPercent = 20;
     [SerializeField] public GameObject itemRow;
-    private GameObject previousRow;
+    [SerializeField] public int spawnHeight = 10;
+
+    [Header("")]
+    [SerializeField] public float rowSpeed = 1f;
+    [SerializeField] public float speedIncreaseRate = 0.1f;
+    [SerializeField] public float maxRowSpeed = 8f;
+
+    [Header("")]
+    [SerializeField] public float hazardSpawnPercent = 20f;
+    [SerializeField] public float hazardSpawnIncreaseRate = 0.1f;
+    [SerializeField] public float maxHazardSpawnPercent = 20f;
+
+    [Header("")]
+    [SerializeField] public float smallHazardSpawnPercent = 20f;
+    [SerializeField] public float smallHazardSpawnIncreaseRate = 0.1f;
+    [SerializeField] public float maxSmallHazardSpawnPercent = 20f;
+
+    [Header("")]
+    [SerializeField] public float bubbleSpawnPercent = 20f;
+    [SerializeField] public float bubbleSpawnIncreaseRate = 0.1f;
+    [SerializeField] public float maxBubbleSpawnPercent = 20f;
+
+    [Header("")]
+    [SerializeField] public float fishSpawnPercent = 20f;
+    [SerializeField] public float fishSpawnIncreaseRate = 0.1f;
+    [SerializeField] public float maxFishSpawnPercent = 20f;
+
+    [HideInInspector] public GameObject currentRow;
+    [HideInInspector] public GameObject previousRow;
 
     // Start is called before the first frame update
     void Start()
@@ -21,23 +46,28 @@ public class ItemRowSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (previousRow == null)
+        if (currentRow == null)
         {
             return;
         }
 
-        if (previousRow.transform.position.y <= (spawnHeight - 2))
+        if (currentRow.transform.position.y <= (spawnHeight - 2))
         {
             spawnRow();
         }
+
+        rowSpeed += (rowSpeed >= maxRowSpeed) ? 0f : speedIncreaseRate * Time.deltaTime;
+        hazardSpawnPercent += (hazardSpawnPercent >= maxHazardSpawnPercent) ? 0f : hazardSpawnIncreaseRate * Time.deltaTime;
+        smallHazardSpawnPercent += (smallHazardSpawnPercent >= maxSmallHazardSpawnPercent) ? 0f : smallHazardSpawnIncreaseRate * Time.deltaTime;
+        bubbleSpawnPercent += (bubbleSpawnPercent >= maxBubbleSpawnPercent) ? 0f : bubbleSpawnIncreaseRate * Time.deltaTime;
+        fishSpawnPercent += (fishSpawnPercent >= maxFishSpawnPercent) ? 0f : fishSpawnIncreaseRate * Time.deltaTime;
     }
 
 
     public void spawnRow()
     {
-        previousRow = Instantiate(itemRow, new Vector3(0f, spawnHeight, 0f), transform.rotation);
-        ItemRow row = previousRow.GetComponent<ItemRow>();
-        row.rowSpeed = rowSpeed;
-        row.hazardSpawnPercent = hazardSpawnPercent;
+        previousRow = currentRow;
+        currentRow = Instantiate(itemRow, new Vector3(0f, spawnHeight, 0f), transform.rotation, transform);
+        ItemRow row = currentRow.GetComponent<ItemRow>();
     }
 }
