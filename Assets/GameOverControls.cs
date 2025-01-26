10356a0f9b0398057fa0ffcea46693cd2519182b
Booshinater <ahanlon4j@gmail.com>
2025-01-26 16:31:45 -0500

    ballerballer
    shockcoller

--- Assets/GameOverControls.cs
+++ Assets/GameOverControls.cs
@@ -8,9 +8,11 @@ public class GameOverControls : MonoBehaviour
     public void Reset()
     {
         SceneManager.LoadScene(1);
+        Physics2D.IgnoreLayerCollision(6, 6, false);
     }
     public void MainMenu()
     {
+        Physics2D.IgnoreLayerCollision(6, 6, false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
